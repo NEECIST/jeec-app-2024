7bd22aa0dd3ee8f18d582276663e7b4f494372da
Rui Daniel <ruipedrocd@gmail.com>
2024-01-27 23:24:43 +0000

    new stores

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -20,10 +20,14 @@
 <script setup>
 import axios from 'axios';
 import { useUserStore } from '@/stores/UserStore';
+import { usePrizesStore } from '@/stores/PrizesStore';
+import { useNewActivityStore } from '@/stores/NewActivityStore';
 import { decodeCredential } from 'vue3-google-login'
 import CryptoJS from 'crypto-js';
 
 const userStore = useUserStore();
+const userStore2 = useNewActivityStore();
+const userStore3 = usePrizesStore();
 
 const callback = (response) => {
   const userData = decodeCredential(response.credential)
@@ -35,6 +39,8 @@ const callback = (response) => {
       const jwt = decrypt(response.data)
 
       userStore.authUser(jwt)
+      userStore2.authUser(jwt)
+      userStore3.authUser(jwt)
     })
 }
 
