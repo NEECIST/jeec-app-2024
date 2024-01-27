1daa1c677f5405f626e97547977abd7b9bcd0de9
Rui Daniel <ruipedrocd@gmail.com>
2024-01-27 17:56:46 +0000

    nextactivity2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -123,7 +123,8 @@ import { mapState } from 'pinia'
 export default {
   name: "Home",
   computed: {
-    ...mapState(useUserStore, ['user', 'next_activity']),
+    ...mapState(useUserStore, ['user']),
+    ...mapState(useUserStore, ['next_activity'])
   },
   // data: function () {
   //   return {
