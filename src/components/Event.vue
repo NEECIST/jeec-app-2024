a9d9d129e904cbe74462e8ba9dec242966ea0017
JoelWang <joelhenwang@gmail.com>
2024-02-12 10:06:42 +0000

    add event to calendar function

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -25,8 +25,8 @@
         </router-link>
       </div>
 
-      <div class="add" @click="addEvent()">
-        <a>
+      <div class="add" @click="addToCalendar()">
+        <a class="atcb-button">
           <svg id="symbol" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_1105_545)">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M4.375 0C4.54076 0 4.69973 0.065848 4.81694 0.183058C4.93415 0.300269 5 0.45924 5 0.625V1.25H15V0.625C15 0.45924 15.0658 0.300269 15.1831 0.183058C15.3003 0.065848 15.4592 0 15.625 0C15.7908 0 15.9497 0.065848 16.0669 0.183058C16.1842 0.300269 16.25 0.45924 16.25 0.625V1.25H17.5C18.163 1.25 18.7989 1.51339 19.2678 1.98223C19.7366 2.45107 20 3.08696 20 3.75V17.5C20 18.163 19.7366 18.7989 19.2678 19.2678C18.7989 19.7366 18.163 20 17.5 20H2.5C1.83696 20 1.20107 19.7366 0.732233 19.2678C0.263392 18.7989 0 18.163 0 17.5V3.75C0 3.08696 0.263392 2.45107 0.732233 1.98223C1.20107 1.51339 1.83696 1.25 2.5 1.25H3.75V0.625C3.75 0.45924 3.81585 0.300269 3.93306 0.183058C4.05027 0.065848 4.20924 0 4.375 0V0ZM2.5 4.375C2.5 4.03 2.805 3.75 3.18125 3.75H16.8188C17.1938 3.75 17.5 4.03 17.5 4.375V5.625C17.5 5.97 17.195 6.25 16.8175 6.25H3.18125C2.80625 6.25 2.5 5.97 2.5 5.625V4.375ZM10.625 10.625C10.625 10.4592 10.5592 10.3003 10.4419 10.1831C10.3247 10.0658 10.1658 10 10 10C9.83424 10 9.67527 10.0658 9.55806 10.1831C9.44085 10.3003 9.375 10.4592 9.375 10.625V12.5H7.5C7.33424 12.5 7.17527 12.5658 7.05806 12.6831C6.94085 12.8003 6.875 12.9592 6.875 13.125C6.875 13.2908 6.94085 13.4497 7.05806 13.5669C7.17527 13.6842 7.33424 13.75 7.5 13.75H9.375V15.625C9.375 15.7908 9.44085 15.9497 9.55806 16.0669C9.67527 16.1842 9.83424 16.25 10 16.25C10.1658 16.25 10.3247 16.1842 10.4419 16.0669C10.5592 15.9497 10.625 15.7908 10.625 15.625V13.75H12.5C12.6658 13.75 12.8247 13.6842 12.9419 13.5669C13.0592 13.4497 13.125 13.2908 13.125 13.125C13.125 12.9592 13.0592 12.8003 12.9419 12.6831C12.8247 12.5658 12.6658 12.5 12.5 12.5H10.625V10.625Z" fill="#E7E7E7"/>
@@ -67,6 +67,7 @@
 <script>
 import FadeLoop from './FadeLoop.vue';
 import { Carousel, Slide } from 'vue3-carousel';
+import { atcb_action } from 'add-to-calendar-button';
 
 export default {
   name: "Event",
@@ -86,9 +87,17 @@ export default {
     addEvent() {
       console.log("Add event");
     },
-    addToCalendar(){
-
+    formatToCalendarDate(){
+      const date = this.event.day.split(',')[0];
+      const date_components = date.split(' ');
+      const calendarDate = date_components[2] + '-' + date_components[1] + '-' + date_components[0];
+      return calendarDate;
     },
+    addToCalendar(){
+      const atcb_button = document.querySelector(".add");
+      atcb_action(this.atcb_config, atcb_button)
+    },
+
     getImageList(event){
       let image_list = [];
       for (let i = 0; i < event.companies.data.length; i++) {
@@ -101,11 +110,19 @@ export default {
     return {
       image_list: this.getImageList(this.event),
       loadImg: false,
+      atcb_config: {
+        customLabels:{"apple":"Apple Calendar", "google":"Google Calendar", "outlookcom":"Outlook Calendar"},
+        name: "[JEEC] " + this.event.name,
+        description: this.event.description,
+        startDate: this.formatToCalendarDate(),
+        startTime: this.event.time,
+        endTime: this.event.end_time,
+        options: ["Google", "Apple", "Outlook.com"],
+      }
     };
   },
   mounted (){
-    this.loadImg = true
-
+    this.loadImg = true;
   }
 
 };
