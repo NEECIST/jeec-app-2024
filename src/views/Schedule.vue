d849982a96fefc2cbceedeec76ccbc5aef7f48b3
deco <andrefsantos@skiff.com>
2024-02-14 22:29:51 +0000

    schedule debug 3

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -143,25 +143,25 @@
         <div class="carousel" style="margin-bottom: 110px;">
           <Carousel ref="schedule_carousel" :mouseDrag="false" :touchDrag="false" :itemsToShow="2.5" :wrapAround="true" :transition="500">
             <Slide v-for="(weekday, index) in weekdays" :key="index">
-              <!-- <div style="flex-direction: column;">
+              <div style="flex-direction: column;">
                 <button  class="carousel__item" style="cursor: pointer; margin-bottom: 10px;" @click="carouselSlideEvent($event.target.parentElement.parentElement)">
                   <div class="weekday" >
                     <p style="pointer-events: none;">{{ weekday }}</p>
                   </div>
-                </button> -->
+                </button>
 
-                <h2>ola</h2>
-  
+                
                 <!-- Weekday's Schedule -->
-                <!-- <div class="carousel_item">
-                  <div class="schedule">
+                <div class="carousel_item">
+                  <h2>ola</h2>
+                  <!-- <div class="schedule">
                     <div class="line"></div>
                     <div v-for="(event, index) in activities" :key="event" class="event">
                       <Event v-if="getWeekday(event.day) == weekday" color="aliceblue" :event="event" :index="weekday+index" link="/home"></Event>
                     </div>
-                  </div> 
+                  </div>  -->
                 </div>
-              </div> -->
+              </div>
             </Slide>
             <div class="spacer"></div>
           </Carousel> 
